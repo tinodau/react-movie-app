d5a314c68543ebd15ced20a3e4c9b43e517b6a0d
Daulat Rachmanto <daulat.rachmanto@gmail.com>
2019-05-07 14:36:33 +0700

    better movies in teathers

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,7 +3,15 @@ import axios from "axios";
 import { Menu } from "../../components";
 import { BASE_URL, KEY, POSTER_PORTRAIT_BIG } from "../../services/api";
 
-import { Grid, GridList, GridListTile, Paper } from "@material-ui/core";
+import {
+  Grid,
+  GridList,
+  GridListTile,
+  GridListTileBar,
+  Paper,
+  Typography,
+  Divider
+} from "@material-ui/core";
 
 import "./home.css";
 
@@ -20,7 +28,6 @@ class Home extends Component {
     axios
       .get(`${BASE_URL}/movie/now_playing?${KEY}&language=en-US&page=1`)
       .then(res => {
-        console.log(res.data.results);
         const moviesInTheathers = res.data.results;
         this.setState({ moviesInTheathers });
       });
@@ -34,19 +41,27 @@ class Home extends Component {
           <Grid container spacing={24}>
             <Grid item xs={12}>
               <Paper className="body-padding">
-                {/* {this.state.moviesInTheathers.map(movie => (
-                  <li key={movie.id}>{movie.original_title}</li>
-                ))} */}
-                <GridList cellHeight={160} cols={3}>
-                  {this.state.moviesInTheathers.map(tile => (
+                {/* Movies in Teathers */}
+                <Typography color="inherit" className="in-teathers-title">
+                  In Teathers
+                </Typography>
+                <GridList cellHeight={300} cols={5}>
+                  {this.state.moviesInTheathers.slice(0, 5).map(tile => (
                     <GridListTile key={tile.id} cols={tile.cols || 1}>
                       <img
                         src={`${POSTER_PORTRAIT_BIG + tile.poster_path}`}
                         alt={tile.title}
                       />
+                      <GridListTileBar title={tile.title} />
                     </GridListTile>
                   ))}
                 </GridList>
+                <Typography color="inherit" className="in-teathers-more">
+                  More
+                </Typography>
+                {/* Movies in Teathers */}
+
+                <Divider variant="middle" />
               </Paper>
             </Grid>
           </Grid>
--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,13 +1,19 @@
 const KEY = "api_key=4bd3b71ae20e0bccb619c9d3a71ee33a";
 const BASE_URL = "https://api.themoviedb.org/3";
 const MOVIE_NOW_PLAYING = "movie/now_playing";
+const POSTER_ORIGINAL = "https://image.tmdb.org/t/p/original/";
 const POSTER_PORTRAIT_SMALL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2";
 const POSTER_PORTRAIT_BIG = "https://image.tmdb.org/t/p/w600_and_h900_bestv2";
+const POSTER_LANDSCAPE_SMALL = "https://image.tmdb.org/t/p/w250_and_h141_face/";
+const POSTER_LANDSCAPE_BIG = "https://image.tmdb.org/t/p/w533_and_h300_bestv2/";
 
 export {
   KEY,
   BASE_URL,
   MOVIE_NOW_PLAYING,
+  POSTER_ORIGINAL,
   POSTER_PORTRAIT_SMALL,
-  POSTER_PORTRAIT_BIG
+  POSTER_PORTRAIT_BIG,
+  POSTER_LANDSCAPE_SMALL,
+  POSTER_LANDSCAPE_BIG
 };
